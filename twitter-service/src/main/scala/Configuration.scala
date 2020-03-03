df0420fe8fa1092f0fd8db1e9f0ba416381efb69
Alexander Onbysh <alexander.onbysh@gmail.com>
2020-03-03 02:24:55 -0700

    update twitter producer

--- twitter-service/src/main/scala/Configuration.scala
+++ twitter-service/src/main/scala/Configuration.scala
@@ -1,17 +1,14 @@
 import java.util
 
 class Configuration {
+  val env: util.Map[String, String] = System.getenv()
 
-  class Configuration {
-    val env: util.Map[String, String] = System.getenv()
-
-    val bootstrap: String = env.getOrDefault("BOOTSTRAP_URL", "localhost:9092")
-    val outputTopic: String = env.get("OUTPUT_TOPIC", "test")
-    //    keys
-    val ConsumerKey: String = env.get("CONSUMER_KEY")
-    val ConsumerSecret: String = env.get("CONSUMER_SECRET")
-    val AccessToken: String = env.get("ACCESS_TOKEN")
-    val AccessTokenSecret: String = env.get("ACCESS_TOKEN_SECRET")
-  }
-
+  val bootstrap: String = env.getOrDefault("BOOTSTRAP_URL", "localhost:9092")
+  val outputTopic: String = env.getOrDefault("OUTPUT_TOPIC", "test")
+  //    keys
+  val ConsumerKey: String = env.get("CONSUMER_KEY")
+  val ConsumerSecret: String = env.get("CONSUMER_SECRET")
+  val AccessToken: String = env.get("ACCESS_TOKEN")
+  val AccessTokenSecret: String = env.get("ACCESS_TOKEN_SECRET")
 }
+
--- twitter-service/src/main/scala/DataProducer.scala
+++ twitter-service/src/main/scala/DataProducer.scala
@@ -7,23 +7,22 @@ import java.util.Properties
 
 import com.danielasfregola.twitter4s.TwitterStreamingClient
 import com.danielasfregola.twitter4s.entities.streaming.StreamingMessage
-import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken, Tweet}
+import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken, Coordinate, GeoBoundingBox, Tweet}
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.kafka.clients.producer._
 
-class CompletionCallback extends Callback {
-  override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit =
-    println(s"Complete ${metadata.timestamp()}")
-}
 
 object KafkaProducer extends App with LazyLogging {
+  val configuration = new Configuration()
+
   val props = new Properties()
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, configuration.bootstrap)
   props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
   props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
   val producer = new KafkaProducer[String, String](props)
-  val TOPIC = "t2"
+  val topic = configuration.outputTopic
   logger.info("Kafka producer initialized")
 
   val consumerToken = ConsumerToken(key = "my-consumer-key", secret = "my-consumer-secret")
@@ -32,16 +31,17 @@ object KafkaProducer extends App with LazyLogging {
   val client = TwitterStreamingClient(consumerToken, accessToken)
   logger.info("Twitter client initialized")
 
-//  def printTweetText: PartialFunction[StreamingMessage, Unit] = {
-//    case tweet: Tweet => println(tweet.text)
-//  }
-//
-//  client.sampleStatuses(stall_warnings = true)(printTweetText)
 
-    producer.send(new ProducerRecord(TOPIC, "key", s"Start: ${LocalDateTime.now()}"))
-    for (_ <- 1 to 10)
-      producer.send(new ProducerRecord(TOPIC, "key", s"Message ${LocalDateTime.now()}"))
-    producer.send(new ProducerRecord(TOPIC, "key", s"End: ${LocalDateTime.now()}"), new CompletionCallback())
+  def tweetToStream: PartialFunction[StreamingMessage, Unit] = {
+    case tweet: Tweet =>
+      producer.send(new ProducerRecord(topic, "key", s"Message ${LocalDateTime.now()}"))
+      logger.debug(tweet.text)
+  }
 
-    producer.close()
+  client.filterStatuses(
+    //    all tweets with Tesla
+    tracks = List("Tesla"),
+    //    San Francisco
+    locations = List(GeoBoundingBox(Coordinate(-122.75, 36.8), Coordinate(-121.75, 37.8))),
+  )(tweetToStream)
 }
