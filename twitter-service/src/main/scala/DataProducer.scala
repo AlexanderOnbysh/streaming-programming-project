90c3ae99f61a15650397b91afb29fedef37cd786
Alexander Onbysh <alexander.onbysh@gmail.com>
2020-03-09 16:15:26 +0200

    fix configuration in twitter service

--- twitter-service/src/main/scala/DataProducer.scala
+++ twitter-service/src/main/scala/DataProducer.scala
@@ -1,9 +1,9 @@
-import java.time.LocalDateTime
 import java.util.Properties
 
 import com.danielasfregola.twitter4s.TwitterStreamingClient
+import com.danielasfregola.twitter4s.entities.enums.Language.English
 import com.danielasfregola.twitter4s.entities.streaming.StreamingMessage
-import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken, Coordinate, GeoBoundingBox, Tweet}
+import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken, Tweet}
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.kafka.clients.producer._
 
@@ -21,8 +21,8 @@ object KafkaProducer extends App with LazyLogging {
   val topic = configuration.outputTopic
   logger.info("Kafka producer initialized")
 
-  val consumerToken = ConsumerToken(key = "my-consumer-key", secret = "my-consumer-secret")
-  val accessToken = AccessToken(key = "my-access-key", secret = "my-access-secret")
+  val consumerToken = ConsumerToken(key = configuration.ConsumerKey, secret = configuration.ConsumerSecret)
+  val accessToken = AccessToken(key = configuration.AccessToken, secret = configuration.AccessTokenSecret)
 
   val client = TwitterStreamingClient(consumerToken, accessToken)
   logger.info("Twitter client initialized")
@@ -36,8 +36,8 @@ object KafkaProducer extends App with LazyLogging {
 
   client.filterStatuses(
     //    all tweets with Tesla
-    tracks = List("Tesla"),
+    tracks = List("COVID19"),
     //    San Francisco
-    locations = List(GeoBoundingBox(Coordinate(-122.75, 36.8), Coordinate(-121.75, 37.8))),
+    languages = List(English)
   )(tweetToStream)
 }
