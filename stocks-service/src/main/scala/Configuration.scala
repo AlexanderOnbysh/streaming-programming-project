7d43be57a69cd6c8ab6152845084346be5d1ae4a
Alexander Onbysh <alexander.onbysh@gmail.com>
2020-03-10 09:07:47 +0200

    stock service: update configuration

--- stocks-service/src/main/scala/Configuration.scala
+++ stocks-service/src/main/scala/Configuration.scala
@@ -4,7 +4,7 @@ class Configuration {
   val env: util.Map[String, String] = System.getenv()
 
   val bootstrap: String = env.getOrDefault("BOOTSTRAP_URL", "localhost:9092")
-  val outputTopic: String = env.getOrDefault("OUTPUT_TOPIC", "test2")
+  val outputTopic: String = env.getOrDefault("OUTPUT_TOPIC", "stocks")
   //    creds
   val symbols: Seq[String] = env.getOrDefault("SYMBOLS", "msft").split(",").toSeq
   val token: String = env.get("TOKEN")
